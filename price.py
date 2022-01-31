1bc0bf2228dbf33822ecf8cb719e72ab53e38be4
“AgungMoekti” <agungsaemdoe@gmail.com>
2022-01-31 19:04:48 +0700

    add error except

--- price.py
+++ price.py
@@ -3,10 +3,13 @@ import json
 import sys, os
 
 def price(coin):
+    try:
             api_indodax = requests.get('https://indodax.com/api/tickers')
             raw = api_indodax.json()
             harga = raw['tickers'][coin+'_idr']['buy']
-            vol = raw['tickers'][coin+'_idr']['vol_idr']
+            vol = raw['tickers'][coin+'_idr']['vol_idr']      
             print(f'\r{chr(62)}Harga {coin} Saat Ini : Rp{int(harga):,}')
             print(f'\r{chr(62)}Volume Market : Rp{int(vol):,}')
+    except:   
+            print("No such coin name " + coin)
 price(coin=str(input('Masukan Kode Coin : '))) 
